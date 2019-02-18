683e61c6e4acf21b677e3c26aa0fcd0482f558d7
Henry Edwards <tardiszilla@gmail.com>
2019-02-18 11:01:24 -0500

    initial navbar setup, with account options

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
-	before_action :authenticate_user!
+	before_action :configure_permitted_parameters, if: :devise_controller?
+
+	def configure_permitted_parameters
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email])
+		# allow user to change below
+		devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email])
+
+	end
 end
